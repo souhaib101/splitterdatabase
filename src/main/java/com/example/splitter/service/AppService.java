b10c67e357717daf5d051c066cb8fc6a853b2da1
jabal <souhaib.alabdeh@gmail.com>
2023-03-16 15:03:13 +0100

    servic and html

--- src/main/java/com/example/splitter/service/AppService.java
+++ src/main/java/com/example/splitter/service/AppService.java
@@ -1,6 +1,11 @@
 package com.example.splitter.service;
 
+import com.example.splitter.database.dto.group.Groups;
+import com.example.splitter.database.dto.group.Memberref;
+import com.example.splitter.database.dto.user.Member;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
 
 @Service
 public class AppService {
@@ -15,5 +20,20 @@ public class AppService {
         this.groupRepoitory = groupRepoitory;
         this.expenditureRepository = expenditureRepository;
         this.memberRepository = memberRepository;
+    }
+
+    public Member getMember(String handle) {
+        Member member = memberRepository.getMemberByGithub(handle);
+        if (member != null){
+            return member;
+        }
+        return memberRepository.save(new Member(null, handle));
+    }
+
+    public Groups makeGroup(String name, String handle) {
+        Member member = getMember(handle);
+        Groups group = new Groups(null, name, new HashSet<>(), new HashSet<>());
+        group.members().add(new Memberref(member.id()));
+        return groupRepoitory.save(group);
     }
 }
