8eba6605f63ebb5555f5decbde05e48046ff7eec
jabal <souhaib.alabdeh@gmail.com>
2023-03-16 15:02:24 +0100

    refactor tables name

--- src/main/resources/db/migration/V1__table.sql
+++ src/main/resources/db/migration/V1__table.sql
@@ -1,46 +1,46 @@
 
-create table if not exists "Member"
+create table if not exists Member
 (
     id     serial
         primary key,
-    github varchar(40)
+    github varchar(4000)
 );
 
-create table if not exists "Expenditure"
+create table if not exists Expenditure
 (
   id serial primary key ,
   expense integer,
   payer integer
 );
 
-create table if not exists "Group"
+create table if not exists Groups
 (
     id serial primary key,
     name varchar(40)
 );
 
-create table if not exists "Memberref"
+create table if not exists Memberref
 (
     id integer,
-    groupId integer,
-    foreign key (groupId) references "Group"(id),
-    primary key (id, groupId)
+    groups integer,
+    foreign key (groups) references Groups(id),
+    primary key (id, groups)
 );
 
-create table if not exists "Expenditureref"
+create table if not exists Expenditureref
 (
     id integer,
-    groupId integer,
-     foreign key (groupId) references "Group"(id),
-     primary key (id, groupId)
+    groups integer,
+     foreign key (groups) references Groups(id),
+     primary key (id, groups)
 );
 
-create table if not exists "Involved_Party"
+create table if not exists Involved_Party
 (
     id integer,
-    expenditureId integer,
-    foreign key (expenditureId) references "Member"(id),
-    primary key (id, expenditureId)
+    expenditure integer,
+    foreign key (expenditure) references Member(id),
+    primary key (id, expenditure)
 );
 
 
