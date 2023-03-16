ebe7e0a021272025bf1462d8399dacc49cef6033
jabal <souhaib.alabdeh@gmail.com>
2023-03-16 14:12:53 +0100

    ModellAttribute handle name of GitHub

--- src/main/java/com/example/splitter/controllers/MemberController.java
+++ src/main/java/com/example/splitter/controllers/MemberController.java
@@ -1,8 +1,13 @@
 package com.example.splitter.controllers;
 
+import com.example.splitter.database.dto.user.Member;
 import com.example.splitter.service.AppService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.security.Principal;
 
 @Controller
 public class MemberController {
@@ -12,9 +17,16 @@ public class MemberController {
         this.service = service;
     }
 
+    @ModelAttribute("handle")
+    public String handle(Principal user) {
+        return user.getName();
+    }
 
     @GetMapping("/")
-    public String getHome(){
+    public String getHome(@ModelAttribute("handle") String handle, Model model){
+        Member member = service.getMember(handle);
+        System.out.println("githubename = "+handle);
+        model.addAttribute("member", member);
         return "index";
     }
 
