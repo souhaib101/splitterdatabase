2552b54f374a22c4eedcf5d9a5391706dd07ab56
jabal <souhaib.alabdeh@gmail.com>
2023-03-19 14:19:14 +0100

    Bug: space between ':' and 'memberId'

--- src/main/java/com/example/splitter/database/dao/GroupRepository.java
+++ src/main/java/com/example/splitter/database/dao/GroupRepository.java
@@ -5,10 +5,11 @@ import com.example.splitter.database.dto.group.Groups;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
+
 import java.util.List;
 
 public interface GroupRepository extends CrudRepository<Groups, Long> {
 
-    @Query("SELECT * FROM groups WHERE id IN (SELECT groups FROM memberref WHERE memberref.id =: memberId)")
+    @Query("SELECT * FROM groups WHERE id IN (SELECT groups FROM memberref WHERE memberref.id = :memberId);")
     List<Groups> findGroupsForMember(Long memberId);
 }
